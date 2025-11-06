cd0673178c5efd1b5218ca2451f34eb5ab1c36fb
Dan547918 <mugambiduncan45@gmail.com>
2025-11-06 12:15:54 +0300

    CAT2_Q1.c

--- CAT2_Q1.c
+++ CAT2_Q1.c
@@ -1,18 +1,12 @@
 #include <stdio.h>
 
-
-
-
 int main() {
 
     // initialize 2*2 integer array
 
     int array[2][2] = {{65, 92}, {84, 72}};
 
-
-
-
-    // nested for loop
+  // nested for loop
 
     for (int i = 0; i < 2; i++) {
 
