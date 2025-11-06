4dc9e32a1192324904b1adb044b30cd94ce73e56
Dan547918 <mugambiduncan45@gmail.com>
2025-11-06 12:11:54 +0300

    Add files via upload

---
+++ CAT2_Q1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+
+
+
+int main() {
+
+    // initialize 2*2 integer array
+
+    int array[2][2] = {{65, 92}, {84, 72}};
+
+
+
+
+    // nested for loop
+
+    for (int i = 0; i < 2; i++) {
+
+        for (int j = 0; j < 2; j++) {
+
+            printf("%d\n", array[i][j]);
+
+      }
+  }
+
+    return 0;
+
+}
+
+
